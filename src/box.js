810674f7dec43623afe4b8ce6260fde365e349bf
svekai <karinsve90@hotmail.com>
2020-09-24 14:15:16 +0200

    initial commit

---
+++ src/box.js
@@ -0,0 +1,3 @@
+export function getBox(color) {
+  return { color: color };
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import "./styles.css";
+import { getBox } from "./box.js";
 
-var boxJson = {
-  color: "blue",
-  visible: true
-};
 var box =
   '<div style="border: solid 3px black; background-color:' +
-  boxJson.color +
-  '; height:25px; width:25px;"></div>';
+  getBox("blue").color +
+  '; height:25px; width:25px;">' +
+  "</div>";
 
 document.getElementById("app").innerHTML = box + box;
